f9da63089779c8de8c9968a72972ab3f8af6da65
Nicolas Santa <nicolas55ar@gmail.com>
2011-01-26 15:00:40 -0300

    created the first helper methods

--- lib/fbdialogs.rb
+++ lib/fbdialogs.rb
@@ -1,20 +1,29 @@
 module FBDialogs
   
-  BASE_URL = {:mobile => "http://m.facebook.com/dialog/" , 
-              :desktop => "http://www.facebook.com/dialog/" }.freeze
+  BASE_URL = {:mobile => "http://m.facebook.com/dialog" , 
+              :desktop => "http://www.facebook.com/dialog" }.freeze
   
-  def feed
+  def self.feed(app_id, redirect_uri, options = {} , device = :desktop)
+    build_dialog_url(app_id, redirect_uri,device,'feed',options)
   end
   
-  def friends
+  def self.friends(app_id, redirect_uri, options = {} , device = :desktop)
+    build_dialog_url(app_id, redirect_uri,device,'friends',options)
   end
   
   
-  def oauth
+  def self.oauth(app_id, redirect_uri, options = {} , device = :desktop)
+    build_dialog_url(app_id, redirect_uri,device,'oauth',options)
   end
   
-  def pay
+  def self.pay(app_id, redirect_uri, options = {} , device = :desktop)
+    build_dialog_url(app_id, redirect_uri,device,'pay',options)
   end
   
+  
+  def self.build_dialog_url(app_id, redirect_uri,device,dialog,options)
+    options.merge!(:app_id => app_id , :redirect_uri => redirect_uri)
+    [[BASE_URL[device],dialog].join('/') , options.to_a.map{|p| p.join('=')}.join('&')].join('?')
+  end
   
 end

1a0b62f00d7e0b8334820fee052a4156cc5d1f3b
Nicolas Santa <nicolas55ar@gmail.com>
2011-01-26 14:22:12 -0300

    add base skeleton

---
+++ lib/fbdialogs.rb
@@ -0,0 +1,20 @@
+module FBDialogs
+  
+  BASE_URL = {:mobile => "http://m.facebook.com/dialog/" , 
+              :desktop => "http://www.facebook.com/dialog/" }.freeze
+  
+  def feed
+  end
+  
+  def friends
+  end
+  
+  
+  def oauth
+  end
+  
+  def pay
+  end
+  
+  
+end
